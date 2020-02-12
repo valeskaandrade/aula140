e22e81de271546a7e69aeefcc16cf22b1a35beae
“valeskaandrade” <valeska.andrade@gmail.com>
2020-02-12 19:52:28 -0300

    try-catch programa entra 2 numeros inteiros e faz erros de entrada de dados para ver comportamento

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace TesteTryCatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                
+                int n1 = int.Parse(Console.ReadLine());
+                int n2 = int.Parse(Console.ReadLine());
+                int result = n1 / n2;
+                Console.WriteLine(result);
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Divisão por zero");
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("Erro de formatação: " + e.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Erro: " + e.Message);
+            }
+
+        }
+    }
+}
